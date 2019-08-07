c052405dfcab2f1d35a836091c6a85e34d278c99
jenniemillard <50451732+jenniemillard@users.noreply.github.com>
2019-08-07 11:44:54 -0300

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,5 @@
  </head>
  <body>
  <?php echo '<h1><p><b>HandsOn OpenShift!!!!</b></p></h1>'; ?> 
-  <imag src="D:\Documentos\logos\logos\logos_coloridas.zip\PNG_RGB - laranja\OIOI.0004.008.AF.160105.Logomarca_RGB_Laranja_3I.png" alt="Oi Simples Assim" />
-  <img >
- </body>
+  </body>
 </html>
